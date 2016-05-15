82acfc4f082604c7d268f41be5e60215ccedaf63
AnonymousRookie <357688981@qq.com>
2016-05-15 16:51:48 +0800

    Add cpp_astyle_lint tools & git hook to pre-commit

---
+++ tools/cpp_astyle_lint/cpp_astyle_lint_changed_codes.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+if git rev-parse --verify HEAD >/dev/null 2>&1
+then
+	against=HEAD
+else
+	# Initial commit: diff against an empty tree object
+	against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+fi
+
+# We should pass only added or modified C/C++ source files to cppcheck.
+changed_files=$(git diff-index --cached $against | \
+	grep -E '[MA]	.*\.(c|cpp|h)$' | cut -d'	' -f 2)
+
+astyle_args="--style=google --suffix=none --indent=spaces=4 --min-conditional-indent=2 --align-pointer=type --align-reference=type --indent-switches --indent-cases --indent-col1-comments --pad-oper --pad-header --unpad-paren --close-templates --convert-tabs --mode=c"
+ignore_lists="-legal/copyright,-whitespace/line_length"
+
+cpp_astyle_lint_Dir="./tools/cpp_astyle_lint"
+
+cp $cpp_astyle_lint_Dir/astyle $cpp_astyle_lint_Dir/astyle.exe
+
+	
+if [ -n "$changed_files" ]; then
+	$cpp_astyle_lint_Dir/astyle.exe $astyle_args $changed_files
+	python $cpp_astyle_lint_Dir/cpplint.py --filter=$ignore_lists $changed_files
+	exit $?
+fi
